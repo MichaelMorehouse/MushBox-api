a46b31f2719f5212c8e91315eabaec6f21a8769d
Michael M <mimomiller@gmail.com>
2018-12-16 14:44:04 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+const logger = require('./logger.js')
+
+app.use(bodyParser.json())
+
+app.get('/data', (req, res) => res.send('hi thar'))
+app.post('/data', (req, res) => {
+    logger.log(req.body)
+    res.send(req.body)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
