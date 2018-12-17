83f06e37fc2240f2e898e23327d397152ef3268b
Michael M <mimomiller@gmail.com>
2018-12-16 19:37:10 -0800

    database config

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 3000
+const { Pool, Client } = require('pg')
+const connectionString = process.env.DATABASE_URL
 
 const logger = require('./logger.js')
 
@@ -11,6 +13,15 @@ app.get('/data', (req, res) => res.send('hi thar'))
 app.post('/data', (req, res) => {
     logger.log(req.body)
     res.send(req.body)
+})
+
+console.log(connectionString)
+
+const pool = new Pool()
+
+pool.query('SELECT NOW()', (err, res) => {
+    console.log(err, res)
+    pool.end()
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
