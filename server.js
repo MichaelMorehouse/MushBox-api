2f7bea2b09fa97c5bbd06b0e4226b8856958ebaf
Michael M <mimomiller@gmail.com>
2018-12-17 08:14:26 -0800

    create table text

--- server.js
+++ server.js
@@ -19,48 +19,22 @@ const pool = new Pool({
     connectionString: connectionString,
 })
   
-pool.connect((err, client, done) => {
+const createTableText = () => `
+CREATE TEMP TABLE dates(
+  date_col DATE,
+  timestamp_col TIMESTAMP,
+  timestamptz_col TIMESTAMPTZ,
+);
+`
 
-    const shouldAbort = (err) => {
-      if (err) {
-        console.error('Error in transaction', err.stack)
-        client.query('ROLLBACK', (err) => {
-          if (err) {
-            console.error('Error rolling back client', err.stack)
-          }
-          // release the client back to the pool
-          done()
-        })
-      }
-      return !!err
+(async () => {
+    const client = await pool.connect()
+    try {
+      await client.query(createTableText)
+      console.log("we did it boss")
+    } finally {
+      client.release()
     }
+  })().catch(e => console.log(e.stack))
 
-    const createTableText = `
-        CREATE TEMP TABLE dates(
-        date_col DATE,
-        timestamp_col TIMESTAMP,
-        timestamptz_col TIMESTAMPTZ,
-    );
-    `
-    // create our temp table
-    client.query(createTableText, (err) => {
-        if (shouldAbort(err)) return
-    })
-
-    // insert the current time into it
-    const now = new Date()
-    const insertText = 'INSERT INTO dates(date_col, timestamp_col, timestamtz_col'
-    client.query(insertText, [now, now, now], (err) => {
-        if (shouldAbort(err)) return
-    })
-
-    // read the row back out
-    const result = client.query('SELECT * FROM dates', err => {
-        if (shouldAbort(err)) return
-    })
-
-    console.log(result.rows)
-    done()
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
