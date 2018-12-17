9b22354ebf264ca135762ca1ac732665a6306d0a
Michael M <mimomiller@gmail.com>
2018-12-17 08:25:51 -0800

    create table route

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const logger = require('./logger.js')
 app.use(bodyParser.json())
 
 app.get('/data', (req, res) => res.send('hi thar'))
+
+app.get('/create', (req, res) => {
+    createTable(req, res);
+    res.send('tables boss');
+})
+
 app.post('/data', (req, res) => {
     logger.log(req.body)
     res.send(req.body)
@@ -18,7 +24,7 @@ app.post('/data', (req, res) => {
 const pool = new Pool({
     connectionString: connectionString,
 })
-  
+
 const createTableText = () => `
 CREATE TEMP TABLE dates(
   date_col DATE,
@@ -27,14 +33,20 @@ CREATE TEMP TABLE dates(
 );
 `
 
-(async () => {
+const createTable = async (req, res) => {
     const client = await pool.connect()
+    console.log("trying boss")
     try {
       await client.query(createTableText)
       console.log("we did it boss")
-    } finally {
+    } 
+    catch (err) {
+        console.log(err.stack)
+    } 
+    finally {
       client.release()
     }
-  })().catch(e => console.log(e.stack))
+}
 
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
